8b862d71eb0783b5458297f62de9b69e3c8d5762
Isuru Fernando <isuruf@gmail.com>
2018-11-14 22:04:53 -0600

    remove C++17

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-export CFLAGS="-O2 -g -I$PREFIX/include $CFLAGS"
-export CXXFLAGS="-O2 -g -I$PREFIX/include $CXXFLAGS"
-
-mkdir -p gslcblas
-mv "$PREFIX"/lib/libgslcblas.* gslcblas/
-
-if [ "$(uname)" == "Darwin" ]
-then
-    ln -s "$PREFIX/lib/libopenblas.dylib" "$PREFIX/lib/libgslcblas.dylib"
-    ln -s "$PREFIX/lib/libopenblas.dylib" "$PREFIX/lib/libgslcblas.0.dylib"
-elif [ "$(uname)" == "Linux" ]
-then
-    ln -s "$PREFIX/lib/libopenblas.so" "$PREFIX/lib/libgslcblas.so"
-    ln -s "$PREFIX/lib/libopenblas.so" "$PREFIX/lib/libgslcblas.so.0"
-fi
+export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++11/g")
+export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++14/-std=c++11/g")
 
 chmod +x configure
 ./configure --prefix="$PREFIX" --disable-gui --disable-ao
@@ -26,5 +11,3 @@ make -j${CPU_COUNT}
 # Enable patches/nofltk-check.patch as well
 # make check -j${CPU_COUNT}
 make install
-
-mv gslcblas/* "$PREFIX/lib/" 
