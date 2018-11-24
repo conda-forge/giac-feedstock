01589f335bceb2411f52b0e38f49d48ceec09c82
Isuru Fernando <isuruf@gmail.com>
2018-11-24 01:36:17 -0600

    Fix cast

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++11/g")
 export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++14/-std=c++11/g")
 
 # Newer clang doesn't like converting non constant values in initializer lists
-sed -i.bak "s/{order,lexvars}/{(short)order,(short)lexvars}/g" src/solve.cc
+sed -i.bak "s/{order,lexvars}/{(short)order,(unsigned char)lexvars}/g" src/solve.cc
 sed -i.bak "s/{order.val,0}/{(short)order.val,0}/g" src/solve.cc
 
 chmod +x configure
